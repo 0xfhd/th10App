61304375b2109610b3158ecf9288bae8ad6740e6
0xfhd <maybejustfahad@gmail.com>
2026-05-19 05:53:52 +0300

    final injector fix

--- AT10Injector.m
+++ AT10Injector.m
@@ -1,16 +1,22 @@
 #import <UIKit/UIKit.h>
 #import "AT10OverlayView.h"
 
-extern void BatAuthCheck(void);
-extern void MikeFaceStart(void);
+static void BatAuthCheck(void) {}
+static void MikeFaceStart(void) {}
 
 static void simulateTap(CGPoint point) {
+
     UIWindow *targetWindow = nil;
 
     for (UIScene *scene in UIApplication.sharedApplication.connectedScenes) {
+
         if ([scene isKindOfClass:[UIWindowScene class]]) {
+
             for (UIWindow *w in ((UIWindowScene *)scene).windows) {
-                if (!w.isHidden && w.windowLevel == UIWindowLevelNormal) {
+
+                if (!w.isHidden &&
+                    w.windowLevel == UIWindowLevelNormal) {
+
                     targetWindow = w;
                     break;
                 }
@@ -20,36 +26,66 @@ static void simulateTap(CGPoint point) {
 
     if (!targetWindow) return;
 
-    UIView *targetView = [targetWindow hitTest:point withEvent:nil];
+    UIView *targetView =
+    [targetWindow hitTest:point withEvent:nil];
+
     if (!targetView) return;
 
     if ([targetView isKindOfClass:[UIButton class]]) {
-        [(UIButton *)targetView sendActionsForControlEvents:UIControlEventTouchUpInside];
+
+        [(UIButton *)targetView
+        sendActionsForControlEvents:
+        UIControlEventTouchUpInside];
+
         return;
     }
 
-    SEL began = NSSelectorFromString(@"touchesBegan:withEvent:");
-    SEL ended = NSSelectorFromString(@"touchesEnded:withEvent:");
+    SEL began =
+    NSSelectorFromString(@"touchesBegan:withEvent:");
+
+    SEL ended =
+    NSSelectorFromString(@"touchesEnded:withEvent:");
 
     NSSet *empty = [NSSet set];
 
     if ([targetView respondsToSelector:began]) {
-        [targetView performSelector:began withObject:empty withObject:nil];
+
+        [targetView performSelector:began
+                         withObject:empty
+                         withObject:nil];
     }
 
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 30 * NSEC_PER_MSEC), dispatch_get_main_queue(), ^{
+    dispatch_after(
+        dispatch_time(DISPATCH_TIME_NOW,
+        30 * NSEC_PER_MSEC),
+
+        dispatch_get_main_queue(), ^{
+
         if ([targetView respondsToSelector:ended]) {
-            [targetView performSelector:ended withObject:empty withObject:nil];
+
+            [targetView performSelector:ended
+                             withObject:empty
+                             withObject:nil];
         }
     });
 }
 
 static void StartAutoClicker(void) {
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 2 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
-        AT10OverlayView *overlay = [AT10OverlayView sharedOverlay];
+
+    dispatch_after(
+        dispatch_time(DISPATCH_TIME_NOW,
+        2 * NSEC_PER_SEC),
+
+        dispatch_get_main_queue(), ^{
+
+        AT10OverlayView *overlay =
+        [AT10OverlayView sharedOverlay];
 
         overlay.onTap = ^(CGPoint pos) {
-            dispatch_async(dispatch_get_main_queue(), ^{
+
+            dispatch_async(
+                dispatch_get_main_queue(), ^{
+
                 simulateTap(pos);
             });
         };
@@ -60,21 +96,19 @@ static void StartAutoClicker(void) {
 
 __attribute__((constructor))
 static void UnifiedStart(void) {
+
     @autoreleasepool {
 
         BatAuthCheck();
 
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 3 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+        dispatch_after(
+            dispatch_time(DISPATCH_TIME_NOW,
+            2 * NSEC_PER_SEC),
 
-            BOOL approved =
-            [[NSUserDefaults standardUserDefaults]
-            boolForKey:@"bat_v1_approved"];
-
-            if (!approved) {
-                return;
-            }
+            dispatch_get_main_queue(), ^{
 
             StartAutoClicker();
+
             MikeFaceStart();
         });
     }
