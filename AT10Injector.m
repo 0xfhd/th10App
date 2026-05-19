007d27b5c53e6aa5d72efbae14cad873f41796bd
0xfhd <maybejustfahad@gmail.com>
2026-05-19 07:29:14 +0300

    fix auto clicker export

--- AT10Injector.m
+++ AT10Injector.m
@@ -1,80 +1,11 @@
 #import <UIKit/UIKit.h>
 #import "AT10OverlayView.h"
 
-static void BatAuthCheck(void) {}
-static void MikeFaceStart(void) {}
-
-static void simulateTap(CGPoint point) {
-
-    UIWindow *targetWindow = nil;
-
-    for (UIScene *scene in UIApplication.sharedApplication.connectedScenes) {
-
-        if ([scene isKindOfClass:[UIWindowScene class]]) {
-
-            for (UIWindow *w in ((UIWindowScene *)scene).windows) {
-
-                if (!w.isHidden &&
-                    w.windowLevel == UIWindowLevelNormal) {
-
-                    targetWindow = w;
-                    break;
-                }
-            }
-        }
-    }
-
-    if (!targetWindow) return;
-
-    UIView *targetView =
-    [targetWindow hitTest:point withEvent:nil];
-
-    if (!targetView) return;
-
-    if ([targetView isKindOfClass:[UIButton class]]) {
-
-        [(UIButton *)targetView
-        sendActionsForControlEvents:
-        UIControlEventTouchUpInside];
-
-        return;
-    }
-
-    SEL began =
-    NSSelectorFromString(@"touchesBegan:withEvent:");
-
-    SEL ended =
-    NSSelectorFromString(@"touchesEnded:withEvent:");
-
-    NSSet *empty = [NSSet set];
-
-    if ([targetView respondsToSelector:began]) {
-
-        [targetView performSelector:began
-                         withObject:empty
-                         withObject:nil];
-    }
+void StartAutoClicker(void) {
 
     dispatch_after(
         dispatch_time(DISPATCH_TIME_NOW,
-        30 * NSEC_PER_MSEC),
-
-        dispatch_get_main_queue(), ^{
-
-        if ([targetView respondsToSelector:ended]) {
-
-            [targetView performSelector:ended
-                             withObject:empty
-                             withObject:nil];
-        }
-    });
-}
-
-static void StartAutoClicker(void) {
-
-    dispatch_after(
-        dispatch_time(DISPATCH_TIME_NOW,
-        2 * NSEC_PER_SEC),
+        1 * NSEC_PER_SEC),
 
         dispatch_get_main_queue(), ^{
 
@@ -83,33 +14,74 @@ static void StartAutoClicker(void) {
 
         overlay.onTap = ^(CGPoint pos) {
 
-            dispatch_async(
+            UIWindow *targetWindow = nil;
+
+            for (UIScene *scene in UIApplication.sharedApplication.connectedScenes) {
+
+                if ([scene isKindOfClass:[UIWindowScene class]]) {
+
+                    for (UIWindow *w in ((UIWindowScene *)scene).windows) {
+
+                        if (!w.isHidden &&
+                            w.windowLevel == UIWindowLevelNormal) {
+
+                            targetWindow = w;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            if (!targetWindow) return;
+
+            UIView *target =
+            [targetWindow hitTest:pos
+                        withEvent:nil];
+
+            if (!target) return;
+
+            if ([target isKindOfClass:[UIButton class]]) {
+
+                [(UIButton *)target
+                sendActionsForControlEvents:
+                UIControlEventTouchUpInside];
+
+                return;
+            }
+
+            SEL began =
+            NSSelectorFromString(
+            @"touchesBegan:withEvent:");
+
+            SEL ended =
+            NSSelectorFromString(
+            @"touchesEnded:withEvent:");
+
+            NSSet *empty =
+            [NSSet set];
+
+            if ([target respondsToSelector:began]) {
+
+                [target performSelector:began
+                             withObject:empty
+                             withObject:nil];
+            }
+
+            dispatch_after(
+                dispatch_time(DISPATCH_TIME_NOW,
+                40 * NSEC_PER_MSEC),
+
                 dispatch_get_main_queue(), ^{
 
-                simulateTap(pos);
+                if ([target respondsToSelector:ended]) {
+
+                    [target performSelector:ended
+                                 withObject:empty
+                                 withObject:nil];
+                }
             });
         };
 
         [overlay showInView:nil];
     });
-}
-
-__attribute__((constructor))
-static void UnifiedStart(void) {
-
-    @autoreleasepool {
-
-        BatAuthCheck();
-
-        dispatch_after(
-            dispatch_time(DISPATCH_TIME_NOW,
-            2 * NSEC_PER_SEC),
-
-            dispatch_get_main_queue(), ^{
-
-            StartAutoClicker();
-
-            MikeFaceStart();
-        });
-    }
 }
