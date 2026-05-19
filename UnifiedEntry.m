a9e9d625079fe9f6a14d31cf4536dbfb31fb3731
0xfhd <maybejustfahad@gmail.com>
2026-05-19 07:26:55 +0300

    fix real module names

--- UnifiedEntry.m
+++ UnifiedEntry.m
@@ -1,42 +1,49 @@
 #import <UIKit/UIKit.h>
 #import <QuartzCore/QuartzCore.h>
-#import <dlfcn.h>
+
+extern void BatAuthCheck(void);
+
+extern void StartAutoClicker(void);
+extern void StartMikeFaceModule(void);
+extern void StartMicUnlockModule(void);
+extern void StartUnifiedSpeedBoost(void);
 
 extern void StartUnifiedMenu(void);
 
-typedef void (*VoidFunc)(void);
-
-static void CallIfExists(const char *name) {
-    VoidFunc fn = (VoidFunc)dlsym(RTLD_DEFAULT, name);
-    if (fn) {
-        fn();
-    }
-}
-
-static void RunAuthFirst(void) {
-    CallIfExists("StartProtection");
-    CallIfExists("BatAuthCheck");
-}
-
-static void RunModules(void) {
-    CallIfExists("StartAutoClicker");
-    CallIfExists("StartAnimationBooster");
-    CallIfExists("MikeFaceStart");
-    CallIfExists("MicUnlockStart");
-
-    StartUnifiedMenu();
-}
-
 __attribute__((constructor))
 static void UnifiedStart(void) {
-    @autoreleasepool {
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 2 * NSEC_PER_SEC),
-        dispatch_get_main_queue(), ^{
-            RunAuthFirst();
 
-            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 2 * NSEC_PER_SEC),
+    @autoreleasepool {
+
+        dispatch_after(
+            dispatch_time(DISPATCH_TIME_NOW,
+            1 * NSEC_PER_SEC),
+
             dispatch_get_main_queue(), ^{
-                RunModules();
+
+            // الحماية أولاً
+            BatAuthCheck();
+
+            dispatch_after(
+                dispatch_time(DISPATCH_TIME_NOW,
+                2 * NSEC_PER_SEC),
+
+                dispatch_get_main_queue(), ^{
+
+                // الأوتو كلكر
+                StartAutoClicker();
+
+                // MikeFace
+                StartMikeFaceModule();
+
+                // MicUnlock
+                StartMicUnlockModule();
+
+                // Speed
+                StartUnifiedSpeedBoost();
+
+                // القائمة
+                StartUnifiedMenu();
             });
         });
     }
